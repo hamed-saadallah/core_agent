72ee84b7d0df5cf169c553f6112de7cd62e4c07f
hamed saadallah <hamed.saadallah@gmail.com>
2026-04-20 00:07:48 +0200

    fix

---
+++ backend/src/config/migrations/1776636450547-InitialSchema.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class InitialSchema1776636450547 implements MigrationInterface {
+    name = 'InitialSchema1776636450547'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "models" ALTER COLUMN "temperature" SET DEFAULT '0.7'`);
+        await queryRunner.query(`ALTER TABLE "agents" ALTER COLUMN "temperature" SET DEFAULT '0.7'`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "agents" ALTER COLUMN "temperature" SET DEFAULT 0.7`);
+        await queryRunner.query(`ALTER TABLE "models" ALTER COLUMN "temperature" SET DEFAULT 0.7`);
+    }
+
+}
