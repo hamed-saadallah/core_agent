cfc20ed6df4fb4fb4e0638925086622e6d298b9e
hamed saadallah <hamed.saadallah@gmail.com>
2026-04-21 20:15:29 +0200

    fix

--- backend/src/config/migrations/1776636450547-InitialSchema.ts
+++ backend/src/config/migrations/1776636450547-InitialSchema.ts
@@ -4,13 +4,17 @@ export class InitialSchema1776636450547 implements MigrationInterface {
     name = 'InitialSchema1776636450547'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "models" ALTER COLUMN "temperature" SET DEFAULT '0.7'`);
         await queryRunner.query(`ALTER TABLE "agents" ALTER COLUMN "temperature" SET DEFAULT '0.7'`);
+        await queryRunner.query(`ALTER TABLE "models" ALTER COLUMN "temperature" SET DEFAULT '0.7'`);
+        await queryRunner.query(`ALTER TABLE "tools" ALTER COLUMN "isActive" SET DEFAULT true`);
+        await queryRunner.query(`ALTER TABLE "prompts" ALTER COLUMN "isActive" SET DEFAULT true`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "agents" ALTER COLUMN "temperature" SET DEFAULT 0.7`);
         await queryRunner.query(`ALTER TABLE "models" ALTER COLUMN "temperature" SET DEFAULT 0.7`);
+        await queryRunner.query(`ALTER TABLE "tools" ALTER COLUMN "isActive" SET DEFAULT false`);
+        await queryRunner.query(`ALTER TABLE "prompts" ALTER COLUMN "isActive" SET DEFAULT false`);
     }
 
 }
